de106f9e6a8484269f03db8a88e64d0b74279ecc
f-a-tasrif <firozat99@gmail.com>
2026-09-21 20:27:16 +0600

    Remove unused debug and solver scripts to clean up the repository

--- inspect_levels.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from levels_data import build_level1
-from blocks import CodeBlock
-
-lvl = build_level1()
-vals = [o for o in lvl.dynamic_objects if isinstance(o, CodeBlock) and o.kind == 'VALUE']
-print([(o.x, o.y, o.value) for o in vals])
-print('unique_positions', len({(o.x, o.y) for o in vals}))
